b17b5c3fd3f0648eae90c76440c0c031045e3459
naveen <naveen@LAPTOP-CSQMVUNH>
2021-05-24 18:49:48 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,93 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, render_template, request
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+import pickle
+
+# load the nlp model and tfidf vectorizer from disk
+filename = 'nlp_model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+vectorizer = pickle.load(open('tranform.pkl','rb'))
+
+def create_sim():
+    data = pd.read_csv('final_data.csv')
+    # creating a count matrix
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(data['comb'])
+    # creating a similarity score matrix
+    sim = cosine_similarity(count_matrix)
+    return data,sim
+
+
+def rcmd(m):
+    m = m.lower()
+    # check if data and sim are already assigned
+    try:
+        data.head()
+        sim.shape
+    except:
+        data, sim = create_sim()
+    # check if the movie is in our database or not
+    if m not in data['movie_title'].unique():
+        return('Sorry! This movie is not in our database. Please check the spelling or try with some other movies')
+    else:
+        # getting the index of the movie in the dataframe
+        i = data.loc[data['movie_title']==m].index[0]
+
+        # fetching the row containing similarity scores of the movie
+        # from similarity matrix and enumerate it
+        lst = list(enumerate(sim[i]))
+
+        # sorting this list in decreasing order based on the similarity score
+        lst = sorted(lst, key = lambda x:x[1] ,reverse=True)
+
+        # taking top 1- movie scores
+        # not taking the first index since it is the same movie
+        lst = lst[1:11]
+
+        # making an empty list that will containg all 10 movie recommendations
+        l = []
+        for i in range(len(lst)):
+            a = lst[i][0]
+            l.append(data['movie_title'][a])
+        return l
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/recommend")
+def recommend():
+    movie = request.args.get('movie')
+    r = rcmd(movie)
+    movie = movie.upper()
+    if type(r)==type('string'):
+        return render_template('recommend.html',movie=movie,r=r,t='s')
+    else:
+        return render_template('recommend.html',movie=movie,r=r,t='l')
+
+@app.route("/review")
+def review():
+    review = request.args.get('review')
+    reviews_list = [] # list of reviews
+    reviews_status = [] # list of comments (good or bad)
+# if review.string:
+    reviews_list.append(review)
+    # passing the review to our model
+    movie_review_list = np.array([review])
+    movie_vector = vectorizer.transform(movie_review_list)
+    pred = clf.predict(movie_vector)
+    reviews_status.append('Good' if pred else 'Bad')
+
+    # combining reviews and comments into a dictionary
+    movie_reviews = {reviews_list[i]: reviews_status[i] for i in range(len(reviews_list))}
+    print(movie_reviews)
+    return render_template('review.html',reviews=movie_reviews)
+
+if __name__ == '__main__':
+    app.run(debug=True)
